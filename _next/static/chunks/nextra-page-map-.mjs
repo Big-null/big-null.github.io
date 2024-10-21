9c59a6a3f9cce0b35f0a820d0cf241c6b363f78f
r48n34 <r48n34@gmail.com>
2024-10-21 07:34:02 +0000

    Deploying to gh-pages from @ Big-null/big-null.github.io@16f12854417d8460a6aaf23fc798b10696c91c3b 🚀

---
+++ _next/static/chunks/nextra-page-map-.mjs
@@ -0,0 +1,211 @@
+import meta from "../../../pages/_meta.ts";
+export const pageMap = [{
+  data: meta
+}, {
+  name: "index",
+  route: "/",
+  frontMatter: {
+    "sidebarTitle": "Index"
+  }
+}, {
+  name: "lifeIsPossible",
+  route: "/lifeIsPossible",
+  children: [{
+    name: "Lecture",
+    route: "/lifeIsPossible/Lecture",
+    children: [{
+      name: "lecture-01",
+      route: "/lifeIsPossible/Lecture/lecture-01",
+      children: [{
+        name: "lecture-01",
+        route: "/lifeIsPossible/Lecture/lecture-01/lecture-01",
+        frontMatter: {
+          "marp": true,
+          "class": "invert",
+          "footer": "Life is Possible - Apps workshop",
+          "paginate": true,
+          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
+          "sidebarTitle": "Lecture 01"
+        }
+      }]
+    }, {
+      name: "lecture-02",
+      route: "/lifeIsPossible/Lecture/lecture-02",
+      children: [{
+        name: "lecture-02-chinese",
+        route: "/lifeIsPossible/Lecture/lecture-02/lecture-02-chinese",
+        frontMatter: {
+          "marp": true,
+          "class": "invert",
+          "footer": "Life is Possible - Apps workshop",
+          "paginate": true,
+          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
+          "sidebarTitle": "Lecture 02 Chinese"
+        }
+      }, {
+        name: "lecture-02",
+        route: "/lifeIsPossible/Lecture/lecture-02/lecture-02",
+        frontMatter: {
+          "marp": true,
+          "class": "invert",
+          "footer": "Life is Possible - Apps workshop",
+          "paginate": true,
+          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
+          "sidebarTitle": "Lecture 02"
+        }
+      }]
+    }, {
+      name: "lecture-03",
+      route: "/lifeIsPossible/Lecture/lecture-03",
+      children: [{
+        name: "lecture-03-chinese",
+        route: "/lifeIsPossible/Lecture/lecture-03/lecture-03-chinese",
+        frontMatter: {
+          "marp": true,
+          "class": "invert",
+          "footer": "Life is Possible - Apps workshop",
+          "paginate": true,
+          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n\n.columns-three {\n  display: grid;\n  grid-template-columns: repeat(3, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
+          "sidebarTitle": "Lecture 03 Chinese"
+        }
+      }, {
+        name: "lecture-03",
+        route: "/lifeIsPossible/Lecture/lecture-03/lecture-03",
+        frontMatter: {
+          "marp": true,
+          "class": "invert",
+          "footer": "Life is Possible - Apps workshop",
+          "paginate": true,
+          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n\n.columns-three {\n  display: grid;\n  grid-template-columns: repeat(3, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
+          "sidebarTitle": "Lecture 03"
+        }
+      }]
+    }, {
+      name: "lecture-04",
+      route: "/lifeIsPossible/Lecture/lecture-04",
+      children: [{
+        name: "lecture-04-chinese",
+        route: "/lifeIsPossible/Lecture/lecture-04/lecture-04-chinese",
+        frontMatter: {
+          "marp": true,
+          "class": "invert",
+          "footer": "Life is Possible - Apps workshop",
+          "paginate": true,
+          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
+          "sidebarTitle": "Lecture 04 Chinese"
+        }
+      }, {
+        name: "lecture-04",
+        route: "/lifeIsPossible/Lecture/lecture-04/lecture-04",
+        frontMatter: {
+          "marp": true,
+          "class": "invert",
+          "footer": "Life is Possible - Apps workshop",
+          "paginate": true,
+          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
+          "sidebarTitle": "Lecture 04"
+        }
+      }]
+    }, {
+      name: "lecture-05",
+      route: "/lifeIsPossible/Lecture/lecture-05",
+      children: [{
+        name: "lecture-05-chinese",
+        route: "/lifeIsPossible/Lecture/lecture-05/lecture-05-chinese",
+        frontMatter: {
+          "marp": true,
+          "class": "invert",
+          "footer": "Life is Possible - Apps workshop",
+          "paginate": true,
+          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
+          "sidebarTitle": "Lecture 05 Chinese"
+        }
+      }, {
+        name: "lecture-05",
+        route: "/lifeIsPossible/Lecture/lecture-05/lecture-05",
+        frontMatter: {
+          "marp": true,
+          "class": "invert",
+          "footer": "Life is Possible - Apps workshop",
+          "paginate": true,
+          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
+          "sidebarTitle": "Lecture 05"
+        }
+      }]
+    }, {
+      name: "lecture-06",
+      route: "/lifeIsPossible/Lecture/lecture-06",
+      children: [{
+        name: "lecture-06-chinese",
+        route: "/lifeIsPossible/Lecture/lecture-06/lecture-06-chinese",
+        frontMatter: {
+          "marp": true,
+          "class": "invert",
+          "footer": "Life is Possible - Apps workshop",
+          "paginate": true,
+          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
+          "sidebarTitle": "Lecture 06 Chinese"
+        }
+      }, {
+        name: "lecture-06",
+        route: "/lifeIsPossible/Lecture/lecture-06/lecture-06",
+        frontMatter: {
+          "marp": true,
+          "class": "invert",
+          "footer": "Life is Possible - Apps workshop",
+          "paginate": true,
+          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
+          "sidebarTitle": "Lecture 06"
+        }
+      }]
+    }, {
+      name: "lecture-07",
+      route: "/lifeIsPossible/Lecture/lecture-07",
+      children: [{
+        name: "lecture-07-chinese",
+        route: "/lifeIsPossible/Lecture/lecture-07/lecture-07-chinese",
+        frontMatter: {
+          "marp": true,
+          "class": "invert",
+          "footer": "Life is Possible - Apps workshop",
+          "paginate": true,
+          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
+          "sidebarTitle": "Lecture 07 Chinese"
+        }
+      }, {
+        name: "lecture-07",
+        route: "/lifeIsPossible/Lecture/lecture-07/lecture-07",
+        frontMatter: {
+          "marp": true,
+          "class": "invert",
+          "footer": "Life is Possible - Apps workshop",
+          "paginate": true,
+          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
+          "sidebarTitle": "Lecture 07"
+        }
+      }]
+    }]
+  }, {
+    name: "overall-chinese",
+    route: "/lifeIsPossible/overall-chinese",
+    frontMatter: {
+      "marp": true,
+      "class": "invert",
+      "footer": "Life is Possible - Apps workshop",
+      "paginate": true,
+      "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
+      "sidebarTitle": "Overall Chinese"
+    }
+  }, {
+    name: "overall",
+    route: "/lifeIsPossible/overall",
+    frontMatter: {
+      "marp": true,
+      "class": "invert",
+      "footer": "Life is Possible - Apps workshop",
+      "paginate": true,
+      "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
+      "sidebarTitle": "Overall"
+    }
+  }]
+}];
