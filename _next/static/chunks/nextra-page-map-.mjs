f1ef93bb1ae73ab13f3b987fa615ed750626eb6b
r48n34 <r48n34@gmail.com>
2024-12-31 03:53:40 +0000

    Deploying to gh-pages from @ Big-null/big-null.github.io@3cb3983443e378c93f1c19481dd250b3761dba85 🚀

--- _next/static/chunks/nextra-page-map-.mjs
+++ _next/static/chunks/nextra-page-map-.mjs
@@ -12,7 +12,9 @@ export const pageMap = [{
     name: "index",
     route: "/genAI",
     frontMatter: {
-      "sidebarTitle": "Index"
+      "title": "Intro - Techniques for Graphics Generation",
+      "sidebarTitle": "Intro",
+      "description": "Techniques for Graphics Generation introductions"
     }
   }, {
     name: "lecture-01",
@@ -21,7 +23,9 @@ export const pageMap = [{
       name: "lecture-01",
       route: "/genAI/lecture-01/lecture-01",
       frontMatter: {
-        "sidebarTitle": "Lecture 01"
+        "title": "Lecture 1 - Basic prompts for text & code generations",
+        "sidebarTitle": "Lecture 01",
+        "description": "Basic prompts for text & code generations"
       }
     }]
   }, {
@@ -31,7 +35,9 @@ export const pageMap = [{
       name: "lecture-02",
       route: "/genAI/lecture-02/lecture-02",
       frontMatter: {
-        "sidebarTitle": "Lecture 02"
+        "title": "Lecture 2 - Advance prompting in computer vision & video",
+        "sidebarTitle": "Lecture 02",
+        "description": "Advance prompting in computer vision & video"
       }
     }]
   }]
@@ -56,7 +62,9 @@ export const pageMap = [{
         name: "lecture-01",
         route: "/lifeIsPossible/Lecture/lecture-01/lecture-01",
         frontMatter: {
-          "sidebarTitle": "Lecture 01"
+          "title": "Lecture 1 - Introduce to Programming | Life is Possible - 生命教育 手機程式工作坊",
+          "sidebarTitle": "Lecture 01",
+          "description": "Introduce to Programming | Life is Possible - 生命教育 手機程式工作坊"
         }
       }]
     }, {
@@ -66,23 +74,17 @@ export const pageMap = [{
         name: "lecture-02-chinese",
         route: "/lifeIsPossible/Lecture/lecture-02/lecture-02-chinese",
         frontMatter: {
-          "marp": true,
-          "class": "invert",
-          "footer": "Life is Possible - Apps workshop",
-          "paginate": true,
-          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
-          "sidebarTitle": "Lecture 02 Chinese"
+          "title": "講課 2 - Introduce to HTML | Life is Possible - 生命教育 手機程式工作坊",
+          "sidebarTitle": "Lecture 02 Chinese",
+          "description": "Introduce to HTML | Life is Possible - 生命教育 手機程式工作坊"
         }
       }, {
         name: "lecture-02",
         route: "/lifeIsPossible/Lecture/lecture-02/lecture-02",
         frontMatter: {
-          "marp": true,
-          "class": "invert",
-          "footer": "Life is Possible - Apps workshop",
-          "paginate": true,
-          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
-          "sidebarTitle": "Lecture 02"
+          "title": "Lecture 2 - Introduce to HTML | Life is Possible - 生命教育 手機程式工作坊",
+          "sidebarTitle": "Lecture 02",
+          "description": "Introduce to HTML | Life is Possible - 生命教育 手機程式工作坊"
         }
       }]
     }, {
@@ -92,12 +94,9 @@ export const pageMap = [{
         name: "lecture-03-chinese",
         route: "/lifeIsPossible/Lecture/lecture-03/lecture-03-chinese",
         frontMatter: {
-          "marp": true,
-          "class": "invert",
-          "footer": "Life is Possible - Apps workshop",
-          "paginate": true,
-          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n\n.columns-three {\n  display: grid;\n  grid-template-columns: repeat(3, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
-          "sidebarTitle": "Lecture 03 Chinese"
+          "title": "Lecture 3 - Introduce to HTML | Life is Possible - 生命教育 手機程式工作坊",
+          "sidebarTitle": "Lecture 03 Chinese",
+          "description": "Introduce to HTML | Life is Possible - 生命教育 手機程式工作坊"
         }
       }, {
         name: "lecture-03",
@@ -118,23 +117,17 @@ export const pageMap = [{
         name: "lecture-04-chinese",
         route: "/lifeIsPossible/Lecture/lecture-04/lecture-04-chinese",
         frontMatter: {
-          "marp": true,
-          "class": "invert",
-          "footer": "Life is Possible - Apps workshop",
-          "paginate": true,
-          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
-          "sidebarTitle": "Lecture 04 Chinese"
+          "title": "Lecture 4 - Introduce to JavaScript | Life is Possible - 生命教育 手機程式工作坊",
+          "sidebarTitle": "Lecture 04 Chinese",
+          "description": "Introduce to JavaScript | Life is Possible - 生命教育 手機程式工作坊"
         }
       }, {
         name: "lecture-04",
         route: "/lifeIsPossible/Lecture/lecture-04/lecture-04",
         frontMatter: {
-          "marp": true,
-          "class": "invert",
-          "footer": "Life is Possible - Apps workshop",
-          "paginate": true,
-          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
-          "sidebarTitle": "Lecture 04"
+          "title": "Lecture 4 - Introduce to JavaScript | Life is Possible - 生命教育 手機程式工作坊",
+          "sidebarTitle": "Lecture 04",
+          "description": "Introduce to JavaScript | Life is Possible - 生命教育 手機程式工作坊"
         }
       }]
     }, {
@@ -155,12 +148,9 @@ export const pageMap = [{
         name: "lecture-05",
         route: "/lifeIsPossible/Lecture/lecture-05/lecture-05",
         frontMatter: {
-          "marp": true,
-          "class": "invert",
-          "footer": "Life is Possible - Apps workshop",
-          "paginate": true,
-          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
-          "sidebarTitle": "Lecture 05"
+          "title": "Lecture 5 - Interact JS in HTML | Life is Possible - 生命教育 手機程式工作坊",
+          "sidebarTitle": "Lecture 05",
+          "description": "Interact JS in HTML | Life is Possible - 生命教育 手機程式工作坊"
         }
       }]
     }, {
@@ -170,23 +160,17 @@ export const pageMap = [{
         name: "lecture-06-chinese",
         route: "/lifeIsPossible/Lecture/lecture-06/lecture-06-chinese",
         frontMatter: {
-          "marp": true,
-          "class": "invert",
-          "footer": "Life is Possible - Apps workshop",
-          "paginate": true,
-          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
-          "sidebarTitle": "Lecture 06 Chinese"
+          "title": "Lecture 6 - Adv topic - Github, Deployment and Planning | Life is Possible - 生命教育 手機程式工作坊",
+          "sidebarTitle": "Lecture 06 Chinese",
+          "description": "Adv topic - Github, Deployment and Planning | Life is Possible - 生命教育 手機程式工作坊"
         }
       }, {
         name: "lecture-06",
         route: "/lifeIsPossible/Lecture/lecture-06/lecture-06",
         frontMatter: {
-          "marp": true,
-          "class": "invert",
-          "footer": "Life is Possible - Apps workshop",
-          "paginate": true,
-          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
-          "sidebarTitle": "Lecture 06"
+          "title": "Lecture 6 - Adv topic - Github, Deployment and Planning | Life is Possible - 生命教育 手機程式工作坊",
+          "sidebarTitle": "Lecture 06",
+          "description": "Adv topic - Github, Deployment and Planning | Life is Possible - 生命教育 手機程式工作坊"
         }
       }]
     }, {
@@ -196,23 +180,17 @@ export const pageMap = [{
         name: "lecture-07-chinese",
         route: "/lifeIsPossible/Lecture/lecture-07/lecture-07-chinese",
         frontMatter: {
-          "marp": true,
-          "class": "invert",
-          "footer": "Life is Possible - Apps workshop",
-          "paginate": true,
-          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
-          "sidebarTitle": "Lecture 07 Chinese"
+          "title": "講課 7 - Project Week 1 | Life is Possible - 生命教育 手機程式工作坊",
+          "sidebarTitle": "Lecture 07 Chinese",
+          "description": "Project Week 1 | Life is Possible - 生命教育 手機程式工作坊"
         }
       }, {
         name: "lecture-07",
         route: "/lifeIsPossible/Lecture/lecture-07/lecture-07",
         frontMatter: {
-          "marp": true,
-          "class": "invert",
-          "footer": "Life is Possible - Apps workshop",
-          "paginate": true,
-          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
-          "sidebarTitle": "Lecture 07"
+          "title": "Lecture 7 - Project Week 1 | Life is Possible - 生命教育 手機程式工作坊",
+          "sidebarTitle": "Lecture 07",
+          "description": "Project Week 1 | Life is Possible - 生命教育 手機程式工作坊"
         }
       }]
     }, {
@@ -222,12 +200,9 @@ export const pageMap = [{
         name: "lecture-08",
         route: "/lifeIsPossible/Lecture/lecture-08/lecture-08",
         frontMatter: {
-          "marp": true,
-          "class": "invert",
-          "footer": "Life is Possible - Apps workshop",
-          "paginate": true,
-          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
-          "sidebarTitle": "Lecture 08"
+          "title": "Lecture 8 - Project Week 2 | Life is Possible - 生命教育 手機程式工作坊",
+          "sidebarTitle": "Lecture 08",
+          "description": "Project Week 2 | Life is Possible - 生命教育 手機程式工作坊"
         }
       }]
     }, {
@@ -237,12 +212,9 @@ export const pageMap = [{
         name: "lecture-09",
         route: "/lifeIsPossible/Lecture/lecture-09/lecture-09",
         frontMatter: {
-          "marp": true,
-          "class": "invert",
-          "footer": "Life is Possible - Apps workshop",
-          "paginate": true,
-          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
-          "sidebarTitle": "Lecture 09"
+          "title": "Lecture 9 - Project Week 3 | Life is Possible - 生命教育 手機程式工作坊",
+          "sidebarTitle": "Lecture 09",
+          "description": "Project Week 3 | Life is Possible - 生命教育 手機程式工作坊"
         }
       }]
     }, {
@@ -252,12 +224,9 @@ export const pageMap = [{
         name: "lecture-10",
         route: "/lifeIsPossible/Lecture/lecture-10/lecture-10",
         frontMatter: {
-          "marp": true,
-          "class": "invert",
-          "footer": "Life is Possible - Apps workshop",
-          "paginate": true,
-          "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
-          "sidebarTitle": "Lecture 10"
+          "title": "Lecture 10 - Showcase | Life is Possible - 生命教育 手機程式工作坊",
+          "sidebarTitle": "Lecture 10",
+          "description": "Showcase | Life is Possible - 生命教育 手機程式工作坊"
         }
       }]
     }, {
@@ -267,7 +236,9 @@ export const pageMap = [{
         name: "lecture-ex1",
         route: "/lifeIsPossible/Lecture/lecture-ex1/lecture-ex1",
         frontMatter: {
-          "sidebarTitle": "Lecture Ex1"
+          "title": "Lecture ex1 - Advance JS | Life is Possible - 生命教育 手機程式工作坊",
+          "sidebarTitle": "Lecture ex1",
+          "description": "Advance JS | Life is Possible - 生命教育 手機程式工作坊"
         }
       }]
     }]
@@ -275,23 +246,17 @@ export const pageMap = [{
     name: "overall-chinese",
     route: "/lifeIsPossible/overall-chinese",
     frontMatter: {
-      "marp": true,
-      "class": "invert",
-      "footer": "Life is Possible - Apps workshop",
-      "paginate": true,
-      "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
-      "sidebarTitle": "Overall Chinese"
+      "title": "介紹 - Life is Possible - 生命教育 手機程式工作坊",
+      "sidebarTitle": "Overall (中文)",
+      "description": "Life is Possible - 生命教育 手機程式工作坊 介紹"
     }
   }, {
     name: "overall",
     route: "/lifeIsPossible/overall",
     frontMatter: {
-      "marp": true,
-      "class": "invert",
-      "footer": "Life is Possible - Apps workshop",
-      "paginate": true,
-      "style": ".columns {\n  display: grid;\n  grid-template-columns: repeat(2, minmax(0, 1fr));\n  gap: 1rem;\n}\n",
-      "sidebarTitle": "Overall"
+      "title": "Intro - Life is Possible - 生命教育 手機程式工作坊",
+      "sidebarTitle": "Overall",
+      "description": "Life is Possible - 生命教育 手機程式工作坊 introductions"
     }
   }]
 }];
